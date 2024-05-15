893bcd081945bcf3d88ea4ede0bb25e04c39937a
Haichuan Xu <haichuanxu@Haichuans-MacBook-Pro.local>
2024-05-15 17:31:41 -0500

    boa

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const link1 = document.getElementById('link1');
 const link2 = document.getElementById('link2');
 const link3 = document.getElementById('link3');
 const link4 = document.getElementById('link4');
+const link5 = document.getElementById('link5');
 
 link1.addEventListener('click', () => {
     scrollToElement('.header', 0);
@@ -24,4 +25,8 @@ link3.addEventListener('click', () => {
 
 link4.addEventListener('click', () => {
     scrollToElement('.header', 3);
+});
+
+link5.addEventListener('click', () => {
+    scrollToElement('.header', 4);
 });
